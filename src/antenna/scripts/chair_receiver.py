1a7f997fb7056ee96c1a7a538d6ab219a87a4b86
Anonymous1 <felichri@umich.edu>
2021-09-11 15:29:31 -0400

    Added chair_receiver node.

---
+++ src/antenna/scripts/chair_receiver.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import rospy
+import serial
+from std_msgs.msg import String
+
+def chair_receiver():
+    pub = rospy.Publisher("from_chair_receiver", String, queue_size=10)
+    rospy.init_node("chair_receiver", anonymous=True)
+    ser = serial.Serial("/dev/ttyUSB0", 57600)
+    while not rospy.is_shutdown():
+        str_msg = ser.readline()
+        rospy.loginfo(str_msg)
+        pub.publish(str_msg)
+
+if __name__ == '__main__':
+    try:
+        chair_receiver()
+    except rospy.ROSInterruptException:
+        pass
