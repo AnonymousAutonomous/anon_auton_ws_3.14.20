8170679445bd57e3a827d731594d99169ef3e4d6
Fee Christoph <felichri@umich.edu>
2021-10-17 12:16:36 -0400

    cleaned up code

--- Arduino/figure8/figure8.ino
+++ Arduino/figure8/figure8.ino
@@ -2,7 +2,7 @@
 #define IN1 9
 #define IN2 8
 #define ENA 6
-#define RIGHT_MOTOR 6
+#define RIGHT_MOTOR ENA
 
 #define MOTOR_1A 3 // yellow, INTERRUPT
 #define MOTOR_1B 4 // white
@@ -11,96 +11,81 @@
 #define IN3 12
 #define IN4 13
 #define ENB 5
-#define LEFT_MOTOR 5
+#define LEFT_MOTOR ENB
                           
-
 #define MOTOR_2A 2 // yellow, INTERRUPT
 #define MOTOR_2B 7 // white
 
-
-#define FULL_REV_ENCODER_TICKS 4200
-//#define 
-
-// run at 65 in the hallway
-
-
-
-// encoder ticks: 4200 for full revolution
+// define constants
+const int FULL_REV_ENCODER_TICKS = 4200;
+const char FWD = 'f';
+const char BWD = 'b';
 
 volatile long leftEncoderValue = 0;
 volatile long rightEncoderValue = 0;
 
-const int clockwiseLeftFwdSpeed = 150; /* PLACEHOLDER */
-const int clockwiseRightSpeed = 100; /* PLACEHOLDER */
-const long clockwiseLoopDuration = 42000; /* PLACEHOLDER */
+const int clockwiseLeftFwdSpeed = 100; /* PLACEHOLDER */
+const int clockwiseRightSpeed = 90; /* PLACEHOLDER */
+const long clockwiseLoopDuration = 42000; /* in encoder ticks PLACEHOLDER */
 
-const int counterclockwiseLeftSpeed = 130; /* PLACEHOLDER */
-const int counterClockwiseRightFwdSpeed = 122; /* PLACEHOLDER */
-const long counterClockwiseLoopDuration = clockwiseLoopDuration; /* PLACEHOLDER */
+const int counterClockwiseRightFwdSpeed = 100; /* PLACEHOLDER */
+const int counterclockwiseLeftSpeed = 90; /* PLACEHOLDER */
+const long counterClockwiseLoopDuration = clockwiseLoopDuration; /* in encoder ticks PLACEHOLDER */
 
-const int forwardLeftSpeed = 130; /* PLACEHOLDER */
-const int forwardRightSpeed = 105; /* PLACEHOLDER */
+const int forwardLeftSpeed = 85; /* PLACEHOLDER */
+const int forwardRightSpeed = forwardLeftSpeed; /* PLACEHOLDER */
 const int forwardOffset = 0; /* PLACEHOLDER */
 
-const int straightDistance1 = 5000; /* PLACEHOLDER */
-const int straightDistance2 = straightDistance1; /* PLACEHOLDER */
+const int straightDistance1 = 5000; /* in encoder ticks PLACEHOLDER */
+const int straightDistance2 = straightDistance1; /* in encoder ticks PLACEHOLDER */
 
 int state = 0;
 
 
-void right_motor_fwd() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
+void setRightMotorDir(char dir) {
+  if (dir == BWD) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  } else {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+  }
 }
 
-void right_motor_bwd() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-}
-
-void left_motor_fwd() {
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-}
-
-void left_motor_bwd() {
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+void setLeftMotorDir(char dir) {
+  if (dir == BWD) {
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  } else {
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
 }
 
 
-void goStraight(int distance) {
-
-  int rightspeed = forwardRightSpeed;
-  int leftspeed = forwardLeftSpeed;
-
-//  Serial.println(mtrspeed, DEC); 
-//
-//
-//  int rightspeed = mtrspeed;
-//  int leftspeed = mtrspeed;
-  
+// Allow mtrspeed to be overwritten, but set default to -1, which is a flag to use defined speeds up top
+void goStraight(int distance, int mtrspeed = -1) {
+  int rightspeed = mtrspeed == -1 ? forwardRightSpeed : mtrspeed;
+  int leftspeed  = mtrspeed == -1 ? forwardLeftSpeed : mtrspeed;
   
   leftEncoderValue = 0;
   rightEncoderValue = 0;
 
-  right_motor_fwd();
-  left_motor_fwd();
-
-  
+  setLeftMotorDir(FWD);
+  setRightMotorDir(FWD);
 
   while (leftEncoderValue < distance) {
     if (leftEncoderValue < rightEncoderValue) {
-      analogWrite(ENA, rightspeed - forwardOffset);
-      analogWrite(ENB, leftspeed + forwardOffset);
+      analogWrite(RIGHT_MOTOR, rightspeed - forwardOffset);
+      analogWrite(LEFT_MOTOR, leftspeed + forwardOffset);
     }
     else if (leftEncoderValue > rightEncoderValue) {
-      analogWrite(ENA, rightspeed + forwardOffset);
-      analogWrite(ENB, leftspeed - forwardOffset);
+      analogWrite(RIGHT_MOTOR, rightspeed + forwardOffset);
+      analogWrite(LEFT_MOTOR, leftspeed - forwardOffset);
     }
     else {
-      analogWrite(ENA, rightspeed);
-      analogWrite(ENB, leftspeed);
+      analogWrite(RIGHT_MOTOR, rightspeed);
+      analogWrite(LEFT_MOTOR, leftspeed);
     }
   }
 }
@@ -118,8 +103,8 @@ void setup() {
   pinMode(IN4, OUTPUT);
   pinMode(ENB, OUTPUT);
   
-  right_motor_fwd();
-  left_motor_fwd();
+  setLeftMotorDir(FWD);
+  setRightMotorDir(FWD);
 
   //set encoder input from motor 1
   pinMode(MOTOR_1A, INPUT);
@@ -135,44 +120,66 @@ void setup() {
 }
 
 
-void testmotorspeeds() {
-  right_motor_fwd();
-  left_motor_fwd();
+void rampUp() {
+  Serial.println("Ramping motor speeds"); 
+  setLeftMotorDir(FWD);
+  setRightMotorDir(FWD);
+
+  int startSpeed = 50;
+  int endSpeed = 100;
+  int increment = 50;
   
-  for (int i = 70; i < 200; i+=10) {
-    Serial.println(i, DEC); 
-    analogWrite(ENA, i);
-    analogWrite(ENB, i);
+  for (int mtrspeed = startSpeed; mtrspeed <= endSpeed; mtrspeed+=increment) {
+    Serial.println(mtrspeed, DEC); 
+    analogWrite(LEFT_MOTOR, mtrspeed);
+    analogWrite(RIGHT_MOTOR, mtrspeed);
     
-    delay(5000);
-    
+    delay(5000);   
   }
+
+  analogWrite(LEFT_MOTOR, 0);
+  analogWrite(RIGHT_MOTOR, 0);
+  delay(5000);
 }
 
 
-void testspeed(int mtrspeed) {
-
-  left_motor_fwd();
-  right_motor_fwd();
+void testspeed(int mtrspeed, char mtrdir) {
+  setRightMotorDir(mtrdir);
+  setLeftMotorDir(mtrdir);
   
   leftEncoderValue = 0;
+  rightEncoderValue = 0;
   
+  // Test left motor
   unsigned long startTime = millis();
-  while (leftEncoderValue < 5 * FULL_REV_ENCODER_TICKS) {
+
+  if (mtrdir == FWD) {
+    while (leftEncoderValue < 5 * FULL_REV_ENCODER_TICKS) {
       analogWrite(LEFT_MOTOR, mtrspeed);
+    }
+  } else {
+    while (leftEncoderValue >= -5 * FULL_REV_ENCODER_TICKS) {
+      analogWrite(LEFT_MOTOR, mtrspeed);
+    }
   }
-  unsigned long endTime = millis();
   
+  unsigned long endTime = millis();
   analogWrite(LEFT_MOTOR, 0);
 
   unsigned long left_duration = endTime - startTime; 
 
 
-  rightEncoderValue = 0;
+  // Test right motor
   startTime = millis();
   
-  while (rightEncoderValue < 5 * FULL_REV_ENCODER_TICKS) {
+  if (mtrdir == FWD) {
+    while (rightEncoderValue < 5 * FULL_REV_ENCODER_TICKS) {
       analogWrite(RIGHT_MOTOR, mtrspeed);
+    }
+  } else {
+    while (rightEncoderValue >= -5 * FULL_REV_ENCODER_TICKS) {
+      analogWrite(RIGHT_MOTOR, mtrspeed);
+    }
   }
   
   endTime = millis();
@@ -186,57 +193,39 @@ void testspeed(int mtrspeed) {
   Serial.print(left_duration, DEC);
   Serial.print(',');
   Serial.println(right_duration, DEC); 
-  
 }
 
+void goose() {
+  setRightMotorDir(FWD);
+  setLeftMotorDir(FWD);
 
-void loop() {
-
-//  Serial.print(leftEncoderValue, DEC);
-//  Serial.print('\t');
-//  Serial.println(rightEncoderValue, DEC); 
-//
-  for (int i = 50; i <= 200; i += 5) {
-    testspeed(i);
-  }
-
-
-//   testmotorspeeds();
-
-  // 150 to overcome stall?
-
-
-
-  /*
-  right_motor_fwd();
-  left_motor_fwd();
-  // analogWrite(ENA, 255);
-  // analogWrite(ENB, 255);
-
-  analogWrite(ENA, 140);
-  analogWrite(ENB, 140);
+  analogWrite(RIGHT_MOTOR, 140);
+  analogWrite(LEFT_MOTOR, 140);
 
   delay(500);
 
   for (int i = 140; i >= 100; i--) {
-    analogWrite(ENA, i);
-    analogWrite(ENB, i);
+     analogWrite(RIGHT_MOTOR, i);
+     analogWrite(LEFT_MOTOR, i);
   }
 
-  analogWrite(ENA, 100);
-  analogWrite(ENB, 110);
+  analogWrite(RIGHT_MOTOR, 100);
+  analogWrite(LEFT_MOTOR, 110);
 
   delay(10000);
 
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
+  analogWrite(RIGHT_MOTOR, 0);
+  analogWrite(LEFT_MOTOR, 0);
 
   delay(2000);
-*/
 
+}
 
+void figureEight() {
 
-  /* 
+  setRightMotorDir(FWD);
+  setLeftMotorDir(FWD);
+
   // go straight 
   goStraight(straightDistance2);
 
@@ -244,39 +233,70 @@ void loop() {
   leftEncoderValue = 0;
   rightEncoderValue = 0;
 
-  left_motor_fwd();
-  right_motor_bwd();
+  setLeftMotorDir(FWD);
+  setRightMotorDir(BWD);
   
   while (leftEncoderValue < clockwiseLoopDuration) {
-    analogWrite(ENA, clockwiseRightSpeed);
-    analogWrite(ENB, clockwiseLeftFwdSpeed);
+    analogWrite(RIGHT_MOTOR, clockwiseRightSpeed);
+    analogWrite(LEFT_MOTOR, clockwiseLeftFwdSpeed);
     Serial.print(leftEncoderValue, DEC);
     Serial.print('\t');
     Serial.println(rightEncoderValue, DEC); 
   }
   
   // go straight
-  
-
   goStraight(straightDistance1);
 
-  
   // counter clockwise rotation
-
   leftEncoderValue = 0;
   rightEncoderValue = 0;
   
-  left_motor_bwd();
-  right_motor_fwd();
+  setLeftMotorDir(BWD);
+  setRightMotorDir(FWD);
   
   while (rightEncoderValue < counterClockwiseLoopDuration) {
-    analogWrite(ENA, counterClockwiseRightFwdSpeed);
-    analogWrite(ENB, counterclockwiseLeftSpeed);
+    analogWrite(RIGHT_MOTOR, counterClockwiseRightFwdSpeed);
+    analogWrite(LEFT_MOTOR, counterclockwiseLeftSpeed);
   }
-  */
+}
 
-
+void test() {
+  int startSpeed = 50;
+  int endSpeed = 100;
+  int increment = 50;
   
+  Serial.println("fwd_speed,left_ms,right_ms");
+  for (int i = startSpeed; i <= endSpeed; i += increment) {
+    testspeed(i, FWD);
+  }
+  
+  Serial.println("bwd_speed,left_ms,right_ms");
+  for (int i = startSpeed; i <= endSpeed; i += increment) {
+    testspeed(i, BWD);
+  }
+
+  delay(5000);
+}
+
+
+void loop() {
+  // TODO: set mode
+
+  // modes:
+  //    t: test       (collect time for each wheel to complete 5 revolutions at diff speeds)
+  //    f: figure 8   (run figure 8)
+  //    r: ramp up    (ramp up speed with robot going fwd)
+  char mode = 'f';
+
+  if (mode == 't') {
+    test();
+  } else if (mode == 'f') {
+    figureEight();
+  } else if (mode == 'r') {
+    rampUp();
+  } else {
+    Serial.println("Unsupported loop mode. Must be one of: [t, f, r]."); 
+  }
 }
 
 void EncoderEvent1() {
