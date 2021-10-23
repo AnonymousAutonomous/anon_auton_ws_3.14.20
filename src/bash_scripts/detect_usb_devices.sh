a469feaa74ca7783ed58b0548f0e3b22c62eef06
Anonymous1 <felichri@umich.edu>
2021-10-23 16:01:35 -0400

    printing out values

--- src/bash_scripts/detect_usb_devices.sh
+++ src/bash_scripts/detect_usb_devices.sh
@@ -2,6 +2,9 @@
 
 DEV_TO_PORT_FILE="dev_to_port.txt"
 
+# clear file contents
+> $DEV_TO_PORT_FILE
+
 for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
 	(
 		syspath="${sysdevpath%/dev}"
@@ -10,20 +13,19 @@ for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
 		eval "$(udevadm info -q property --export -p $syspath)"
 		[[ -z "$ID_SERIAL" ]] && exit
 		echo "/dev/$devname - $ID_SERIAL"
-		echo $ID_SERIAL
-		[[$ID_SERIAL == *'Arduino'*]] && is_arduino=true || is_arduino=false
-		[[grep -q "Arduino" <<< $ID_SERIAL]] && is_arduino=true || is_arduino=false
 
-		echo $is_arduino
-
-		if [["$ID_SERIAL" == *"Arduino"*]] || [[$ID_SERIAL == *"arduino"*]] || [[$devname == *"ACM"* ]]
-		then
-			echo "ARDUINO"
+		if [[ "$ID_SERIAL" == *"Arduino"* || "$ID_SERIAL" == *"1a86_USB2.0"* ]]; then
+			echo "arduino /dev/$devname" >> $DEV_TO_PORT_FILE
+			export ARDUINO_PORT="/dev/$devname"
+		elif [[ "$ID_SERIAL" == *"Basic_UART"* ]]; then
+			echo "antenna /dev/$devname" >> $DEV_TO_PORT_FILE
+			export ANTENNA_PORT="/dev/$devname"
+		elif [[ "$ID_SERIAL" == *"Silicon_Labs"* ]]; then
+			echo "lidar /dev/$devname" >> $DEV_TO_PORT_FILE
+			export LIDAR_PORT="/dev/$devname"
+		elif [[ "$devname" == *"video"* ]]; then
+			echo "camera /dev/$devname" >> $DEV_TO_PORT_FILE
+			export CAMERA_PORT="/dev/$devname"
 		fi
-
-		if [["$ID_SERIAL" =~ "Arduino" ]]
-		then
-			echo "arduino is here"
-		fi 
 	)
 done
