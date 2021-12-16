07a3f6c6da4c1ee526fabe381bfde46e2f76aa65
froggyhopper50 <jnkennedy32@comcast.net>
2021-12-16 17:56:47 -0500

    first working PID controller

--- Arduino/pid_controller/pid_controller/pid_controller.ino
+++ Arduino/pid_controller/pid_controller/pid_controller.ino
@@ -32,7 +32,7 @@ const uint16_t ENCB = 3;        // Encoder B input         Green    // was 2
 // PWM
 const uint16_t ANALOG_WRITE_BITS = 8;
 const uint16_t MAX_PWM = pow(2, ANALOG_WRITE_BITS)-1;
-const uint16_t MIN_PWM = MAX_PWM / 4;    // Make sure motor turns
+const uint16_t MIN_PWM = 0;    // Let motors stop
 // Motor timing
 unsigned long nowTime = 0;       // updated on every loop
 unsigned long startTimeA = 0;    // start timing A interrupts
@@ -42,19 +42,24 @@ unsigned long countIntB = 0;     // count the B interrupts
 double periodA = 0;              // motor A period
 double periodB = 0;              // motor B period
 // PID 
-const unsigned long SAMPLE_TIME = 100;  // time between PID updates
-const unsigned long INT_COUNT = 20;     // sufficient interrupts for accurate timing
-double setpointA = 50;         // setpoint is rotational speed in Hz
-double inputA = 0;              // input is PWM to motors
-double outputA = 0;             // output is rotational speed in Hz
-double setpointB = 50;         // setpoint is rotational speed in Hz
-double inputB = 0;              // input is PWM to motors
-double outputB = 0;             // output is rotational speed in Hz
-double KpA = 0.50, KiA = 0.00, KdA = 0.0;
-double KpB = 0.50, KiB = 0.00, KdB = 0.0;
+const unsigned long SAMPLE_TIME = 10;  // time between PID updates
+const unsigned long INT_COUNT = 100;     // 100 encoder ticks for accurate timing
+double setpointA = 2.5;         // setpoint is inches / second
+double inputA = 0;              // input is inches / second
+double outputA = 0;             // output is PWM to motors
+double setpointB = setpointA;         // setpoint is inches / second
+double inputB = 0;              // input is inches / second
+double outputB = 0;             // output is PWM to motors
+double KpA = 2.0, KiA = 7.0, KdA = 2.0;
+double KpB = KpA, KiB = KiA, KdB = KdA;
 PID motorA(&inputA, &outputA, &setpointA, KpA, KiA, KdA, DIRECT);
 PID motorB(&inputB, &outputB, &setpointB, KpB, KiB, KdB, DIRECT);
 double storeB = 0;               // used for debug print
+
+double encoderConversion = 1300000.0 / 4200.0; // (10 ticks *  1000 milliseconds * 1 rev * 13 in) / (x millisecond * 1 second * 4200 ticks * 1 rev)
+
+const unsigned int MAX_INPUT = 50;
+
 void setup(){
  initMotors();
  initEncoders();
@@ -74,15 +79,84 @@ void setup(){
 // 
  Serial.print(KpA); Serial.print("  "); Serial.print(KiA); Serial.print("  "); Serial.print(KdA); Serial.print("  ");
  Serial.print(KpB); Serial.print("  "); Serial.print(KiB); Serial.print("  "); Serial.print(KdB); Serial.println("  ");
- Serial.println("inputA  inputB  outputA  outputB  errorA  errorB");
+// Serial.println("inputA  inputB  outputA  outputB  errorA  errorB");
 
 }
+
+void process_data (const char* data) {
+  Serial.print("Processing: "); Serial.print(data); Serial.print("\n");
+  char changeVar = data[0];
+  double changeTo = String(data).substring(1).toFloat();
+  if (changeVar == 'p') {
+    Serial.print("Change KpA and KpB to: "); Serial.print(changeTo); Serial.print("\n");
+    KpA = KpB = changeTo;
+    Serial.print(KpA); Serial.print(" "); Serial.print(KpB); Serial.print("\n");
+  }
+  else if (changeVar == 'i') {
+        KiA = KiB = changeTo;
+
+  }
+  else if (changeVar == 'd') {
+        KdA = KdB = changeTo;
+
+  }
+  else if (changeVar == 's') {
+//         motorA = PID(&inputA, &outputA, &setpointA, KpA, KiA, KdA, DIRECT);
+//        motorB = PID(&inputB, &outputB, &setpointB, KpB, KiB, KdB, DIRECT);
+          // Reset I terms
+          if (setpointA == 0) {
+            
+          
+          motorA.SetOutputLimits(0.0, 1.0);
+                    motorB.SetOutputLimits(0.0, 1.0);
+
+          motorA.SetOutputLimits(-1.0, 0.0);
+          motorB.SetOutputLimits(-1.0, 0.0);
+
+          motorA.SetOutputLimits(MIN_PWM, MAX_PWM);
+          motorB.SetOutputLimits(MIN_PWM, MAX_PWM);
+          }
+                  setpointA = setpointB = changeTo;
+
+  }
+      motorA.SetTunings(KpA, KiA, KdA);
+    motorB.SetTunings(KpB, KiB, KdB);
+}
+
+void processIncomingByte(const byte inByte) {
+  static char input_line [MAX_INPUT];
+  static unsigned int input_pos = 0;
+
+  switch (inByte) {
+    case '\n':
+      input_line[input_pos] = 0; // terminating null byte
+      process_data(input_line);
+      // reset
+      input_pos = 0;
+      break;
+    case '\r':
+      break; 
+    default:
+      if (input_pos < (MAX_INPUT - 1))
+        input_line [input_pos++] = inByte;
+      break;
+  }
+}
+
+
 void loop(){
   nowTime = millis();
   motorA.Compute();
   motorB.Compute();
   forwardA((int)outputA);
   forwardB((int)outputB);
+//  Serial.print("setpoint: "); Serial.print(setpointA); Serial.print(" ");
+//  Serial.print("input:    "); Serial.print(inputA); Serial.print(" ");
+//  Serial.print("output:   "); Serial.print(outputA); Serial.print("\n");
+
+  while (Serial.available() > 0) {
+    processIncomingByte(Serial.read());
+  }
 
 //  Serial.print(countIntA); Serial.print("  ");
 //    Serial.print(countIntB); Serial.println("  ");
@@ -90,16 +164,16 @@ void loop(){
 //  Serial.print(storeB); Serial.print("  "); Serial.print(outputB); Serial.print("  ");
   if (storeB != outputB){
     storeB = outputB;
-    Serial.print(outputA); Serial.print(" "); Serial.print(outputB); Serial.print("\n");
-    
-  }
+//    Serial.print("OUT: "); Serial.print(outputA); Serial.print(" "); Serial.print(outputB); Serial.print("\n");
+//    Serial.print("IN:  "); Serial.print(inputA); Serial.print(" "); Serial.print(inputB); Serial.print("\n");
+//
+//  }
+//    Serial.println("input A, inputB, errorA, errorB");
 //    Serial.print(inputA); Serial.print("  ");
 //    Serial.print(inputB); Serial.print("  ");
-//    Serial.print(outputA); Serial.print("  ");
-//    Serial.print(outputB); Serial.print("  ");
 //    Serial.print(100*(setpointA-inputA)/setpointA); Serial.print("  ");
 //    Serial.print(100*(setpointB-inputB)/setpointB); Serial.println("");
-//  }
+  }
 }
 void forwardA(uint16_t pwm){
   digitalWrite(AIN1, LOW);
@@ -152,10 +226,10 @@ void initMotors(){
   standbyMotors(false);
 }
 void initEncoders(){
-  pinMode(ENCA, INPUT_PULLUP);
-  pinMode(ENCB, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ENCA), isr_A, RISING);
-  attachInterrupt(digitalPinToInterrupt(ENCB), isr_B, RISING);
+  pinMode(ENCA, INPUT);
+  pinMode(ENCB, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCA), isr_A, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCB), isr_B, CHANGE);
 }
 void initPWM(){
   startTimeA = millis();
@@ -173,8 +247,10 @@ void isr_A(){
   // count sufficient interrupts to get accurate timing
   // inputX is the encoder frequency in Hz
   countIntA++;
+//  Serial.println(countIntA);
   if (countIntA == INT_COUNT){
-    inputA = (float) INT_COUNT * 1000 / (float)(nowTime - startTimeA);
+    inputA = (float) encoderConversion * (1.0 / (float)(nowTime - startTimeA));
+//    Serial.println(inputA);
     startTimeA = nowTime;
     countIntA = 0;
   }
@@ -185,7 +261,11 @@ void isr_B(){
   // inputX is the encoder frequency in Hz
   countIntB++;
   if (countIntB == INT_COUNT){
-    inputB = (float) INT_COUNT * 1000 / (float)(nowTime - startTimeB);
+    Serial.println(1.0 / (float)(nowTime - startTimeB));
+    Serial.println(encoderConversion);
+    inputB = (float) encoderConversion * (1.0 / (float)(nowTime - startTimeB));
+
+//    inputB = (float) INT_COUNT * 1000 / (float)(nowTime - startTimeB);
     startTimeB = nowTime;
     countIntB = 0;
   }
