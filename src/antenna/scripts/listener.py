82579b0d3b791b59428094564dc4e5321bdaa3e1
Fee Christoph <felichri@umich.edu>
2022-01-12 15:02:07 -0500

    blank antennas

---
+++ src/antenna/scripts/listener.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import serial
+
+antenna_port = "/dev/ttyUSB0"
+
+if __name__ == '__main__':
+    ser = serial.Serial(antenna_port, 57600)
+
+    while True:
+        str_msg = ser.readline()[:-1]
+        print(str_msg)
+
