4579d8dbca45d07aba76dcb7daeac2c21fcdc633
froggyhopper50 <jnkennedy32@comcast.net>
2022-01-20 15:50:00 -0500

    launch file for debug mode

---
+++ src/launch_manager/launch/launch_autonomous_debug.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd ~/anon_auton_ws
+source devel/setup.bash
+
+/bin/bash ~/anon_auton_ws/src/launch_manager/launch/pre.sh
+
+sleep 5
+
+roslaunch ~/anon_auton_ws/src/launch_manager/launch/autonomous_debug.launch
