fb1b0da602f1d7ff4236a1556dd16e7c9c2178cd
Fee Christoph <felichri@umich.edu>
2022-01-20 21:15:12 -0500

    new pid tuning with feed forward

--- Arduino/pid_controller/pid_controller.ino
+++ Arduino/pid_controller/pid_controller.ino
@@ -13,6 +13,22 @@
  * F Milburn, January 2020
  */
  #include <PID_v1.h>
+
+// ROS connection to pi
+// #include <ros.h>
+// #include <std_msgs/String.h>
+// #include <std_msgs/Int32.h>
+// #include <eyes/Generic.h>
+
+// ros::NodeHandle nh;
+
+// init to zero and update with each encoder tick
+// std_msgs::Int32 int32_msg_R;
+// std_msgs::Int32 int32_msg_L;
+// ros::Publisher pubR("encoder_value_R", &int32_msg_R);
+// ros::Publisher pubL("encoder_value_L", &int32_msg_L);
+
+ 
 // Output pins used to control motors
 
 // A is left motor
@@ -48,21 +64,24 @@ double periodB = 0;              // motor B period
 // PID 
 const unsigned long SAMPLE_TIME = 10;  // time between PID updates
 const unsigned long INT_COUNT = 100;     // 100 encoder ticks for accurate timing
-double setpointA = 0;         // setpoint is inches / second
+
+// !!!!!!!!!!!!! SETPOINTS MUST BE POSITIVE !!!!!!!!!!!!!!
+double setpointA = 2.0;         // setpoint is inches / second
+double setpointB = setpointA;   // setpoint is inches / second
+
 double inputA = 0;              // input is inches / second
 double outputA = 0;             // output is PWM to motors
-int FEEDFWDA = 80;
+int FEEDFWDA = 60;
+int a_adjust = 0;
 
-double setpointB = setpointA;   // setpoint is inches / second
 double inputB = 0;              // input is inches / second
 double outputB = 0;             // output is PWM to motors
-int FEEDFWDB = 80;
-int a_adjust = 0;
+int FEEDFWDB = 60;
 int b_adjust = 0;
 
 //double KpA = 2.0, KiA = 7.0, KdA = 2.0;
 // TODO: diff tunings for fwd or bwd? how to even out between the two motors? -- send avg to both? OR diff FEEDFWD terms for the two?
-double KpA = 1.0, KiA = 1.0, KdA = 0.0;
+double KpA = 10.0, KiA = 25.0, KdA = 0.0;
 double KpB = KpA, KiB = KiA, KdB = KdA;
 PID motorA(&inputA, &outputA, setpointA, KpA, KiA, KdA, DIRECT);
 PID motorB(&inputB, &outputB, setpointB, KpB, KiB, KdB, DIRECT);
@@ -72,113 +91,59 @@ double encoderConversion = 1300000.0 / 4200.0; // (10 ticks *  1000 milliseconds
 
 const unsigned int MAX_INPUT = 50;
 
-void setup(){
- initMotors();
- initEncoders();
- initPWM();
- Serial.begin(115200);
- while(!Serial){
-  // wait for serial to start
+const char FWD = 'f';
+const char BWD = 'r';
+const char STOP = 's';
+
+/***********************************************************
+ * SERIAL / PRINTING HELPER FUNCTIONS                      *
+ ***********************************************************/
+ void printPID() {
+  Serial.print("KpA"); Serial.print(","); Serial.print("KiA"); Serial.print(","); Serial.print("KdA"); Serial.print(","); Serial.print("setpointA"); Serial.print(","); Serial.print("FEEDFWDA"); Serial.print(",");
+  Serial.print("KpB"); Serial.print(","); Serial.print("KiB"); Serial.print(","); Serial.print("KdB"); Serial.print(","); Serial.print("setpointB"); Serial.print(","); Serial.print("FEEDFWDB"); Serial.print("\n");
+  Serial.print(KpA); Serial.print(","); Serial.print(KiA); Serial.print(","); Serial.print(KdA); Serial.print(","); Serial.print(setpointA); Serial.print(","); Serial.print(FEEDFWDA); Serial.print(",");
+  Serial.print(KpB); Serial.print(","); Serial.print(KiB); Serial.print(","); Serial.print(KdB); Serial.print(","); Serial.print(setpointB); Serial.print(","); Serial.print(FEEDFWDB); Serial.print("\n");
+ 
+ Serial.print("inputA"); Serial.print(","); Serial.print("outputA"); Serial.print(","); Serial.print("a_adjust"); Serial.print(","); 
+  Serial.print("inputB"); Serial.print(","); Serial.print("outputB"); Serial.print(","); Serial.print("b_adjust"); Serial.print("\n"); 
  }
 
-// for (int i = 0; i < 40000; i++) {
-//    forwardB(80);
-// }
-
-// for (int i = 0; i < 40000; i++) {
-//  forwardA(150);
-// }
-//
-// for (int i = 0; i < 40000; i++) {
-//  reverseA(150);
-// }
-
-// for (int i = 0; i < 40000; i++) {
-//  reverseB(150);
-// }
-// 
- Serial.print("Kp"); Serial.print(","); Serial.print("Ki"); Serial.print(","); Serial.print("Kd"); Serial.print(","); Serial.print("setpoint"); Serial.print(","); Serial.print("feedfwd"); Serial.print("\n");
- Serial.print(KpA); Serial.print(","); Serial.print(KiA); Serial.print(","); Serial.print(KdA); Serial.print(","); Serial.print(setpointA); Serial.print(","); Serial.print(FEEDFWDA); Serial.print("\n");
- Serial.print("input"); Serial.print(","); Serial.print("output"); Serial.print("\n");
-
-// Serial.print(KpB); Serial.print("  "); Serial.print(KiB); Serial.print("  "); Serial.print(KdB); Serial.println("  ");
-// Serial.println("inputA  inputB  outputA  outputB  errorA  errorB");
-
-}
-
-void process_data (const char* data) {
-  Serial.print("Processing: "); Serial.print(data); Serial.print("\n");
+ void printUpdates() {
+ Serial.print(inputA); Serial.print(","); Serial.print(outputA); Serial.print(","); Serial.print(a_adjust); Serial.print(","); 
+  Serial.print(inputB); Serial.print(","); Serial.print(outputB); Serial.print(","); Serial.print(b_adjust); Serial.print("\n"); 
+ 
+ }
+ 
+ void process_data (const char* data) {
   char changeVar = data[0];
-  double changeTo = String(data).substring(1).toFloat();
   if (changeVar == 'p') {
-    Serial.print("Change KpA and KpB to: "); Serial.print(changeTo); Serial.print("\n");
-    KpA = KpB = changeTo;
-//    Serial.print(KpA); Serial.print(" "); Serial.print(KpB); Serial.print("\n");
+    KpA = KpB = String(data).substring(1).toFloat();
   }
   else if (changeVar == 'i') {
-        KiA = KiB = changeTo;
-        Serial.print("Change KiA and KiB to: "); Serial.print(changeTo); Serial.print("\n");
-
-
+    KiA = KiB = String(data).substring(1).toFloat();
   }
   else if (changeVar == 'd') {
-        KdA = KdB = changeTo;
-        Serial.print("Change KdA and KdB to: "); Serial.print(changeTo); Serial.print("\n");
-
-
+    KdA = KdB = String(data).substring(1).toFloat();
+  }
+  else if (changeVar == 'b') {
+        standbyMotors(true);
   }
   else if (changeVar == 's') {
-//         motorA = PID(&inputA, &outputA, &setpointA, KpA, KiA, KdA, DIRECT);
-//        motorB = PID(&inputB, &outputB, &setpointB, KpB, KiB, KdB, DIRECT);
-          
-          Serial.print("Change setpoint to: "); Serial.print(changeTo); Serial.print("\n");
-
-           // TODO: is this better? 
-           // TODO: need to reset the input because otherwise it's funky. 
-           if (setpointA == 0) {
-            brakeA();
-            brakeB();
-           }
-
-          // TODO: was this working? reset I term for every new setpoint? or just for specific ones?
-          // Reset I terms
-          // motorA.SetOutputLimits(0.0, 1.0);
-          // motorB.SetOutputLimits(0.0, 1.0);
-
-          // motorA.SetOutputLimits(-1.0, 0.0);
-          // motorB.SetOutputLimits(-1.0, 0.0);
-
-          // motorA.SetOutputLimits(MIN_PWM, MAX_PWM);
-          // motorB.SetOutputLimits(MIN_PWM, MAX_PWM);
-          // }
-          setpointA = setpointB = changeTo;
-
-          motorA.SetSetpoint(changeTo);
-          motorB.SetSetpoint(changeTo);
-          a_adjust = setpointA == 0 ? 0 : FEEDFWDA;
-          b_adjust = setpointB == 0 ? 0 : FEEDFWDB;
-
+    parseNewSetpoints(String(data).substring(1));
   }
     motorA.SetTunings(KpA, KiA, KdA);
     motorB.SetTunings(KpB, KiB, KdB);
 
-    Serial.print("Kp"); Serial.print(","); Serial.print("Ki"); Serial.print(","); Serial.print("Kd"); Serial.print(","); Serial.print("setpoint"); Serial.print(","); Serial.print("feedfwd"); Serial.print("\n");
-    Serial.print(KpA); Serial.print(","); Serial.print(KiA); Serial.print(","); Serial.print(KdA); Serial.print(","); Serial.print(setpointA); Serial.print(","); Serial.print(FEEDFWDA); Serial.print("\n");
-    Serial.print("input"); Serial.print(","); Serial.print("output"); Serial.print("\n");
+    printPID();
 
-
-//  Serial.print(KpA); Serial.print(" "); Serial.print(KiA); Serial.print(" "); Serial.print(KdA); Serial.print("\n");
-//  Serial.print(setpointA); Serial.print(" "); Serial.print(a_adjust); Serial.print(" "); Serial.print(outputA); Serial.print("\n");
 }
 
 void processIncomingByte(const byte inByte) {
-//  Serial.print("incoming: " + String(inByte) + "\n");
   static char input_line [MAX_INPUT];
   static unsigned int input_pos = 0;
 
   switch (inByte) {
     case '\n':
-//      Serial.print("NEWLINE\n");
       input_line[input_pos] = 0; // terminating null byte
       process_data(input_line);
       // reset
@@ -193,40 +158,254 @@ void processIncomingByte(const byte inByte) {
   }
 }
 
+/***********************************************************
+ * MOTOR HELPER FUNCTIONS                                  *
+ ***********************************************************/
+void setADir(char dir) {
+  if (dir == BWD) {
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+  }
+  else if (dir == FWD) {
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, HIGH);
+  }
+  else { // STOP / BRAKE
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, LOW);
+  }
+}
+
+void setBDir(char dir) {
+  if (dir == BWD) {
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
+  }
+  else if (dir == FWD) {
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, HIGH);
+  }
+  else { // STOP / BRAKE
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, LOW);
+  }
+}
+
+void moveA(uint16_t pwm){
+  analogWrite(PWMA, pwm);
+}
+
+void moveB(uint16_t pwm){
+  analogWrite(PWMB, pwm);
+}
+
+void standbyMotors(bool standby){
+  if (standby == true){
+    digitalWrite(STBYA, LOW);
+    digitalWrite(STBYB, LOW);
+  }
+  else{
+    digitalWrite(STBYA, HIGH);
+    digitalWrite(STBYB, HIGH);
+  }
+}
+
+// TODO: was this working? reset I term for every new setpoint? or just for specific ones?
+          // Reset I terms
+          // motorA.SetOutputLimits(0.0, 1.0);
+          // motorB.SetOutputLimits(0.0, 1.0);
+
+          // motorA.SetOutputLimits(-1.0, 0.0);
+          // motorB.SetOutputLimits(-1.0, 0.0);
+
+          // motorA.SetOutputLimits(MIN_PWM, MAX_PWM);
+          // motorB.SetOutputLimits(MIN_PWM, MAX_PWM);
+          // }
+
+void setNewSetpointMotorA(float setpoint, char dir) {
+  if (dir == STOP) {
+    a_adjust = 0;
+  }
+  else {
+    a_adjust = FEEDFWDA;
+  }
+  setpointA = setpoint;
+  motorA.SetSetpoint(setpoint);
+  setADir(dir);
+}
+
+void setNewSetpointMotorB(float setpoint, char dir) {
+  if (dir == STOP) {
+    b_adjust = 0;
+  }
+  else {
+    b_adjust = FEEDFWDB;
+  }
+  setpointB = setpoint;
+  motorB.SetSetpoint(setpoint);
+  setBDir(dir);
+}
+
+void parseNewSetpoints(String setpointsIn) {
+  char ADir = '\0';
+  char BDir = '\0';
+  float aSpeed = -1;
+  float bSpeed = -1;
+
+  String temp = "";
+  
+  // Go through string
+  for (char c : setpointsIn) {
+    if (isalpha(c)) {
+      // first alpha = A direction
+      if (ADir == '\0') {
+        ADir = c;
+      }
+      // second alpha = B direction. Set A speed
+      else {
+        BDir = c;
+        aSpeed = temp.toFloat();
+        temp = "";
+        if (aSpeed < 0.0001) {
+          aSpeed = 0;
+          ADir = STOP;
+        }
+      }
+    }
+    // add to temp 
+    else {
+      temp += c;
+    }
+  }
+  // set B Speed
+  bSpeed = temp.toFloat();
+  if (bSpeed < 0.0001) {
+    bSpeed = 0;
+    BDir = STOP;
+  }
+
+  // update setpoints
+  setNewSetpointMotorA(aSpeed, ADir);
+  setNewSetpointMotorB(bSpeed, BDir);
+}
+
+void initMotors(){
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(STBYA, OUTPUT);
+  pinMode(STBYB, OUTPUT);
+  // analogWriteResolution(ANALOG_WRITE_BITS);
+  standbyMotors(false);
+}
+
+void initEncoders(){
+  pinMode(ENCA, INPUT);
+  pinMode(ENCB, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCA), isr_A, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCB), isr_B, CHANGE);
+}
+
+void initPWM(){
+  startTimeA = millis();
+  startTimeB = millis();
+  motorA.SetOutputLimits(MIN_PWM, MAX_PWM);
+  motorB.SetOutputLimits(MIN_PWM, MAX_PWM);
+  motorA.SetSampleTime(SAMPLE_TIME);
+  motorB.SetSampleTime(SAMPLE_TIME);
+  motorA.SetMode(AUTOMATIC);
+  motorB.SetMode(AUTOMATIC);
+  setNewSetpointMotorA(setpointA, FWD);
+  setNewSetpointMotorB(setpointB, FWD);
+} 
+
+
+/***********************************************************
+ * ROS                                                     *
+ ***********************************************************/
+// void generic_callback(const eyes::Generic& generic_msg) {
+//   if (generic_msg.left_forward) {
+//     setLeftMotorDir(FWD);
+//   }
+//   else {
+//     setLeftMotorDir(BWD);
+//   }
+//   if (generic_msg.right_forward) {
+//     setRightMotorDir(FWD);
+//   }
+//   else {
+//     setRightMotorDir(BWD);
+//   }
+//   analogWrite(LEFT_MOTOR, generic_msg.left_speed);
+//   analogWrite(RIGHT_MOTOR, generic_msg.right_speed);
+
+//   return;
+// }
+
+/***********************************************************
+ * SETUP & LOOP                                            *
+ ***********************************************************/
+// ros::Subscriber<eyes::Generic> generic_sub("generic_feed", &generic_callback);
+
+void setup(){
+  // Set up ROS
+  // nh.initNode();
+  // nh.advertise(pubR);
+  // nh.advertise(pubL);
+  // nh.subscribe(generic_sub);
+
+  // Initialize pins and value ranges
+ initMotors();
+ initEncoders();
+ initPWM();
+
+  // Connect to Arduino serial
+ Serial.begin(115200);
+ while(!Serial){
+  // wait for serial to start
+ }
+ 
+//setADir(FWD);
+// setBDir(FWD);
+//
+// for (int i = 100; i >= 0; i-=10) {
+//   Serial.print(i); Serial.print("\n");
+//   moveA(i);
+//   moveB(i);
+//   delay(5000);
+// }
+
+printPID();
+
+}
 
 void loop(){
   nowTime = millis();
   motorA.Compute();
   motorB.Compute();
 
-  forwardA(max(0, min(255, (int)outputA + a_adjust)));
-  forwardB(max(0, min(255, (int)outputB + b_adjust)));
-//  Serial.print("setpoint: "); 
-//  Serial.print(setpointA); Serial.print(" ");
-//  Serial.print("input:    "); 
-  
-//  Serial.print("output:   "); 
-//  Serial.print(inputB); Serial.print(" ");
-
-    
-//  Serial.print("output:   "); 
-//  Serial.print(outputB); Serial.print("\n");
-
-//Serial.print(inputA); Serial.print(",");
-//Serial.print(outputA); Serial.print("\n");
+  moveA(max(0, min(255, (int)outputA + a_adjust)));
+  moveB(max(0, min(255, (int)outputB + b_adjust)));
 
   while (Serial.available() > 0) {
     processIncomingByte(Serial.read());
   }
-  
-Serial.print(inputA); Serial.print(",");
-Serial.print(outputA); Serial.print("\n");
+
+  printUpdates();
+ 
+  // int32_msg_R.data = countR;
+  // int32_msg_L.data = countL;
+  // pubR.publish(&int32_msg_R);
+  // pubL.publish(&int32_msg_L);
+
+  // // TODO: check if this will cause issues
+  // nh.spinOnce();
+  // delay(10);
 
 
-//  Serial.print(countIntA); Serial.print("  ");
-//    Serial.print(countIntB); Serial.println("  ");
-
-//  Serial.print(storeB); Serial.print("  "); Serial.print(outputB); Serial.print("  ");
   if (storeB != outputB){
     storeB = outputB;
 //    Serial.print("OUT: "); Serial.print(outputA); Serial.print(" "); Serial.print(outputB); Serial.print("\n");
@@ -243,99 +422,27 @@ Serial.print(outputA); Serial.print("\n");
 //   Serial.print(inputA); Serial.print("  ");
 
 }
-void forwardA(uint16_t pwm){
-  digitalWrite(AIN1, HIGH);
-  digitalWrite(AIN2, LOW);
-  analogWrite(PWMA, pwm);
-}
-void forwardB(uint16_t pwm){
-  digitalWrite(BIN1, HIGH);
-  digitalWrite(BIN2, LOW);
-  analogWrite(PWMB, pwm);
-}
-void reverseA(uint16_t pwm){
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, HIGH);
-  analogWrite(PWMA, pwm);
-}
-void reverseB(uint16_t pwm){
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, HIGH);  
-  analogWrite(PWMB, pwm);
-}
-void brakeA(){
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, LOW);
-}
-void brakeB(){
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, LOW);
-}
-void standbyMotors(bool standby){
-  if (standby == true){
-    digitalWrite(STBYA, LOW);
-    digitalWrite(STBYB, LOW);
-  }
-  else{
-    digitalWrite(STBYA, HIGH);
-    digitalWrite(STBYB, HIGH);
-  }
-}
-void initMotors(){
-  pinMode(AIN1, OUTPUT);
-  pinMode(AIN2, OUTPUT);
-  pinMode(PWMA, OUTPUT);
-  pinMode(BIN1, OUTPUT);
-  pinMode(BIN2, OUTPUT);
-  pinMode(PWMB, OUTPUT);
-  pinMode(STBYA, OUTPUT);
-  pinMode(STBYB, OUTPUT);
-//  analogWriteResolution(ANALOG_WRITE_BITS);
-  standbyMotors(false);
-}
-void initEncoders(){
-  pinMode(ENCA, INPUT);
-  pinMode(ENCB, INPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCA), isr_A, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENCB), isr_B, CHANGE);
-}
-void initPWM(){
-  startTimeA = millis();
-  startTimeB = millis();
-  motorA.SetOutputLimits(MIN_PWM, MAX_PWM);
-  motorB.SetOutputLimits(MIN_PWM, MAX_PWM);
-  motorA.SetSampleTime(SAMPLE_TIME);
-  motorB.SetSampleTime(SAMPLE_TIME);
-  motorA.SetMode(AUTOMATIC);
-  motorB.SetMode(AUTOMATIC);
-  a_adjust = setpointA == 0 ? 0 : FEEDFWDA;
-  b_adjust = setpointB == 0 ? 0 : FEEDFWDB;
-}
+
+
+
+/***********************************************************
+ * ENCODER CALLBACKS                                       *
+ ***********************************************************/
 void isr_A(){
-//  Serial.print("A"); Serial.print(" "); Serial.println(countIntA);
-//  Serial.println("A ENCODER");
   // count sufficient interrupts to get accurate timing
-  // inputX is the encoder frequency in Hz
   countIntA++;
-//  Serial.println(countIntA);
   if (countIntA == INT_COUNT){
     inputA = (float) encoderConversion * (1.0 / (float)(nowTime - startTimeA));
-//    Serial.println(inputA);
     startTimeA = nowTime;
     countIntA = 0;
   }
 }
+
 void isr_B(){
-//    Serial.print("B"); Serial.print(" "); Serial.println(countIntB);
   // count sufficient interrupts to get accurate timing
-  // inputX is the encoder frequency in Hz
   countIntB++;
   if (countIntB == INT_COUNT){
-//    Serial.println(1.0 / (float)(nowTime - startTimeB));
-//    Serial.println(encoderConversion);
     inputB = (float) encoderConversion * (1.0 / (float)(nowTime - startTimeB));
-
-//    inputB = (float) INT_COUNT * 1000 / (float)(nowTime - startTimeB);
     startTimeB = nowTime;
     countIntB = 0;
   }
