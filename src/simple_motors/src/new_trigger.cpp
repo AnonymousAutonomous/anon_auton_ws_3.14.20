ae850d7ba9cf7b9b7d0331250882812facd643c8
Fee Christoph <felichri@umich.edu>
2022-01-22 16:24:51 -0500

    migrate handwritten to yaml and launch

--- src/simple_motors/src/new_trigger.cpp
+++ src/simple_motors/src/new_trigger.cpp
@@ -2,13 +2,23 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "../../../constants/user_cmds.h"
 #include <string>
 #include <sstream>
+#include <map>
+
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "new_trigger");
   ros::NodeHandle nh;
+  nh.param<std::map<std::string, std::string>> commands;
+
+  if (nh.getParam("handwritten", commands) {
+    ROS_INFO("Handwritten commands have been loaded.");
+  }
+  else {
+    ROS_INFO("You must load handwritten commands first.");
+    return 1;
+  }
 
   ros::Publisher trigger_pub = nh.advertise<std_msgs::String>("driver_output", 1000);
 
@@ -18,47 +28,8 @@ int main(int argc, char** argv) {
 
   while (ros::ok() && std::cin >> cmd && cmd != "exit") {
     std_msgs::String msg;
-    if (cmd == "left") {
-    	msg.data = LEFT;
-    }
-    else if (cmd == "right") {
-    	msg.data = RIGHT;
-    }
-    else if (cmd == "fwd") {
-    	msg.data = FWD;
-    }
-    else if (cmd == "ffwd") {
-    	msg.data = FFWD;
-    }
-    else if (cmd == "fwdl") {
-    	msg.data = FWDL;
-    }
-    else if (cmd == "fwdr") {
-    	msg.data = FWDR;
-    }
-    else if (cmd == "bwd") {
-    	msg.data = BWD;
-    }
-    else if (cmd == "fbwd") {
-    	msg.data = FBWD;
-    }
-    else if (cmd == "bwdl") {
-    	msg.data = BWDL;
-    }
-    else if (cmd == "bwdr") {
-    	msg.data = BWDR;
-    }
-    else if (cmd == "pivotl") {
-    	msg.data = PIVOTL;
-    }
-    else if (cmd == "pivotr") {
-    	msg.data = PIVOTR;
-    }
-    else if (cmd == "toggle") {
-    	msg.data = TOGGLE;
-    }
-    else if (cmd == "stop") {
-    	msg.data = STOP;
+    if (commands.find(cmd) != commands.end()) {
+      msg.data = commands[cmd];
     }
     else {
     	continue;
