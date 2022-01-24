112e2c620f9c4eda7a96babf6e33029aff4de519
Fee Christoph <felichri@umich.edu>
2022-01-24 17:10:23 -0500

    wait until roscore is running

--- src/launch_manager/launch/launch_autonomous.sh
+++ src/launch_manager/launch/launch_autonomous.sh
@@ -6,37 +6,38 @@ source devel/setup.bash
 
 /bin/bash ~/anon_auton_ws/src/launch_manager/launch/pre.sh
 
-sleep 5
+# wait until roscore is running
+until rostopic list ; do sleep 1; done
 
 cd ~/anon_auton_ws/src/launch_manager/launch/components
 
 if grep -iq "^antenna_port" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
     echo "ANTENNA found"
-    roslaunch antenna_chair.launch
+    roslaunch --wait antenna_chair.launch
 else
     echo "!!! NO ANTENNA FOUND !!!"
 fi
 
 if grep -iq "^port" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
     echo "ARDUINO found"
-    roslaunch arduino.launch
+    roslaunch --wait arduino.launch
 else
     echo "!!! NO ARDUINO FOUND !!!"
 fi
 
 if grep -iq "^device_path" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
     echo "CAMERA found"
-    roslaunch camera.launch  
+    roslaunch --wait camera.launch  
 else
     echo "!!! NO CAMERA FOUND !!!"
 fi
 
 if grep -iq "^serial_port" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
     echo "LIDAR found"
-    roslaunch lidar.launch
+    roslaunch --wait lidar.launch
 else
     echo "!!! NO LIDAR FOUND !!!"
 fi
 
 # Launch the queue to start everything
-roslaunch queue.launch
+roslaunch --wait queue.launch
