4a7a1a676c4b2df650425c9ea0f552321afe1bfc
Fee Christoph <felichri@umich.edu>
2022-01-24 17:40:37 -0500

    launch in background so you can launch all the parts

--- src/launch_manager/launch/launch_autonomous.sh
+++ src/launch_manager/launch/launch_autonomous.sh
@@ -13,28 +13,28 @@ cd ~/anon_auton_ws/src/launch_manager/launch/components
 
 if grep -iq "^antenna_port" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
     echo "---------------- ANTENNA found ----------------"
-    roslaunch --wait antenna_chair.launch
+    roslaunch --wait antenna_chair.launch &
 else
     echo "!!! NO ANTENNA FOUND !!!"
 fi
 
 if grep -iq "^port" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
     echo "---------------- ARDUINO found ----------------"
-    roslaunch --wait arduino.launch
+    roslaunch --wait arduino.launch &
 else
     echo "!!! NO ARDUINO FOUND !!!"
 fi
 
 if grep -iq "^device_path" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
     echo "---------------- CAMERA found ----------------"
-    roslaunch --wait camera.launch  
+    roslaunch --wait camera.launch &
 else
     echo "!!! NO CAMERA FOUND !!!"
 fi
 
 if grep -iq "^serial_port" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
     echo "---------------- LIDAR found ----------------"
-    roslaunch --wait lidar.launch
+    roslaunch --wait lidar.launch &
 else
     echo "!!! NO LIDAR FOUND !!!"
 fi
