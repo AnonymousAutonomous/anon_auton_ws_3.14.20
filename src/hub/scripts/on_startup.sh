21e0bc9431dcdc6bb37b650d6a9ab97ae8c04d10
Fee Christoph <felichri@umich.edu>
2022-01-24 19:02:15 -0500

    initial startup script for hub

---
+++ src/hub/scripts/on_startup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd ~/anon_auton_ws
+./src/launch_manager/launch/pre.sh
+
+until rostopic list; do sleep 1; done
+
+if grep -iq "^antenna_port" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
+	echo "----------------- ANTENNA FOUND -----------------"
+	roslaunch --wait src/launch_manager/launch/components/antenna_hub.launch &
+else
+	echo "!!! PLEASE CONNECT THE ANTENNA AND RERUN THIS SCRIPT !!!"
+fi
