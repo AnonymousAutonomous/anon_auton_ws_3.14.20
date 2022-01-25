2de009e8792ee0751e6b082cab36c75584510871
Fee Christoph <felichri@umich.edu>
2022-01-24 20:20:57 -0500

    with popups

--- src/hub/scripts/on_startup.sh
+++ src/hub/scripts/on_startup.sh
@@ -9,7 +9,15 @@ until rostopic list; do sleep 1; done
 
 if grep -iq "^antenna_port" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
 	echo "----------------- ANTENNA FOUND -----------------"
+	notify-send "Antenna is connected."
 	roslaunch --wait src/launch_manager/launch/components/antenna_hub.launch &
 else
-	echo "!!! PLEASE CONNECT THE ANTENNA AND RERUN THIS SCRIPT !!!"
+	notify-send -u critical -t 0 "!!! PLEASE CONNECT THE ANTENNA !!!"
+	until grep -iq "^antenna_port" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml;
+	do
+		./src/launch_manager/launch/pre.sh
+		sleep 5
+	done
+	notify-send "Antenna is connected."
+	roslaunch --wait src/launch_manager/launch/components/antenna_hub.launch &
 fi
