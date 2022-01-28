25e71aa5e7e74c438c91e11a60b50f883ea1d3b1
Fee Christoph <felichri@umich.edu>
2022-01-28 02:54:31 -0500

    migrate to ffplay

--- src/audio/scripts/audio.py
+++ src/audio/scripts/audio.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import rospy
+import subprocess
 from std_msgs.msg import String
 
 import os
@@ -10,6 +11,10 @@ from subprocess import call
 file_dir = os.path.dirname(os.path.abspath(__file__))
 
 chunk = 16 # put 1 here if you want "strobe audio mode" (James)
+
+beep_file = "/../sounds/polite_beep/461679__15hpanska-ruttner-jan__05-horn.wav"
+honk_file = "/../sounds/angry_honk/182474__keweldog__car-horn.wav"
+batt_file = "/../sounds/low_battery/413396__flood-mix__synth-descending-tones-glitchy.wav"
 
 beep_f = wave.open(file_dir + r"/../sounds/polite_beep/461679__15hpanska-ruttner-jan__05-horn.wav", "rb")
 honk_f = wave.open(file_dir + r"/../sounds/angry_honk/182474__keweldog__car-horn.wav", "rb")
@@ -46,42 +51,49 @@ def callback(data):
 	choice = data.data
 
 	if choice == "beep":	
-		while beep_data:
-			beep_stream.write(beep_data)
-			beep_data = beep_f.readframes(chunk)
+        subprocess.Popen("ffplay -nodisp -autoexit " + beep_file + " >/dev/null 2>&1", shell=True)
 
-		beep_f = wave.open(file_dir + r"/../sounds/polite_beep/461679__15hpanska-ruttner-jan__05-horn.wav", "rb")
-		beep_stream = p.open(format = p.get_format_from_width(beep_f.getsampwidth()), channels = beep_f.getnchannels(), rate = beep_f.getframerate(), output = True)
-		beep_data = beep_f.readframes(chunk)
+		# while beep_data:
+		# 	beep_stream.write(beep_data)
+		# 	beep_data = beep_f.readframes(chunk)
+
+		# beep_f = wave.open(file_dir + r"/../sounds/polite_beep/461679__15hpanska-ruttner-jan__05-horn.wav", "rb")
+		# beep_stream = p.open(format = p.get_format_from_width(beep_f.getsampwidth()), channels = beep_f.getnchannels(), rate = beep_f.getframerate(), output = True)
+		# beep_data = beep_f.readframes(chunk)
 
 	elif choice == "honk":
-		while honk_data:
-			honk_stream.write(honk_data)
-			honk_data = honk_f.readframes(chunk)
+		subprocess.Popen("ffplay -nodisp -autoexit " + honk_file + " >/dev/null 2>&1", shell=True)
 
-		honk_f = wave.open(file_dir + r"/../sounds/angry_honk/182474__keweldog__car-horn.wav", "rb")
-		honk_stream = p.open(format = p.get_format_from_width(honk_f.getsampwidth()), channels = honk_f.getnchannels(), rate = honk_f.getframerate(), output = True)
-		honk_data = honk_f.readframes(chunk)
+		# while honk_data:
+		# 	honk_stream.write(honk_data)
+		# 	honk_data = honk_f.readframes(chunk)
+
+		# honk_f = wave.open(file_dir + r"/../sounds/angry_honk/182474__keweldog__car-horn.wav", "rb")
+		# honk_stream = p.open(format = p.get_format_from_width(honk_f.getsampwidth()), channels = honk_f.getnchannels(), rate = honk_f.getframerate(), output = True)
+		# honk_data = honk_f.readframes(chunk)
 
 	elif choice == "batt":
-		while batt_data:
-			batt_stream.write(batt_data)
-			batt_data = batt_f.readframes(chunk)
+		subprocess.Popen("ffplay -nodisp -autoexit " + batt_file + " >/dev/null 2>&1", shell=True)
 
-		batt_f = wave.open(file_dir + r"/../sounds/low_battery/413396__flood-mix__synth-descending-tones-glitchy.wav", "rb")
-		batt_stream = p.open(format = p.get_format_from_width(batt_f.getsampwidth()), channels = batt_f.getnchannels(), rate = batt_f.getframerate(), output = True)
-		batt_data = batt_f.readframes(chunk)	
+		# while batt_data:
+		# 	batt_stream.write(batt_data)
+		# 	batt_data = batt_f.readframes(chunk)
+
+		# batt_f = wave.open(file_dir + r"/../sounds/low_battery/413396__flood-mix__synth-descending-tones-glitchy.wav", "rb")
+		# batt_stream = p.open(format = p.get_format_from_width(batt_f.getsampwidth()), channels = batt_f.getnchannels(), rate = batt_f.getframerate(), output = True)
+		# batt_data = batt_f.readframes(chunk)	
 
 	elif choice == "exit" or choice == "quit":
-		beep_stream.stop_stream()
-		beep_stream.close()	
+		# beep_stream.stop_stream()
+		# beep_stream.close()	
 
-		honk_stream.stop_stream()
-		honk_stream.close()	
+		# honk_stream.stop_stream()
+		# honk_stream.close()	
 
-		batt_stream.stop_stream()
-		batt_stream.close()	
-		p.terminate()
+		# batt_stream.stop_stream()
+		# batt_stream.close()	
+		# p.terminate()
+		pass
 
 	else: 
 		print(f"{choice} is not a valid option. Please try again.")
