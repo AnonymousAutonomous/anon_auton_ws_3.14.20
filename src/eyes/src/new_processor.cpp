855d43ef0bd5ae39dbd0839105d52b109fe73af2
Fee Christoph <felichri@umich.edu>
2022-07-23 16:06:46 -0400

    trying to parameterize

--- src/eyes/src/new_processor.cpp
+++ src/eyes/src/new_processor.cpp
@@ -1,7 +1,7 @@
 // nice
 
 #include <iostream>
-#include <chrono> 
+#include <chrono>
 #include <future>
 
 #include "ros/ros.h"
@@ -14,7 +14,7 @@
 #include <map>
 #include <unordered_map>
 
-ros::Publisher george; //the variable formerly known as signal
+ros::Publisher george; // the variable formerly known as signal
 ros::Publisher debug;
 ros::Publisher notify_lidar;
 
@@ -28,196 +28,244 @@ unsigned int topBandWidth = 120;
 unsigned int sideBandWidth = 180;
 unsigned int imageWidth = 640; // 640 INSTEAD OF 720 IT'S LYING DON'T TRUST IT
 unsigned int imageHeight = 480;
-unsigned int threshold = 175;
-double side_percent_threshold = 0.20;
-double top_percent_threshold = 0.20;
-bool favorRight = false; 
-double num_middle_pixels = 100800;
+unsigned int brightnessThreshold = 175;
+double sidePercentThreshold = 0.20;
+double topPercentThreshold = 0.20;
+bool favorRight = false;
+double numMiddlePixels = 100800;
 
 bool listening = true;
 
-void chatterCallBack(const sensor_msgs::Image& view);
+void chatterCallBack(const sensor_msgs::Image &view);
 void pauseCallback(const std_msgs::Empty empty_msg);
 
 std::map<std::string, std::string> commands_in;
 std::unordered_map<AutonomousCmd, std::string> commands;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "camera_process");
 
     ros::AsyncSpinner spinner(0);
     spinner.start();
 
-    ros::NodeHandle Iris; //subscriber
-    if (Iris.getParam("/autonomous", commands_in)) {
-        for (auto i = commands_in.begin(); i != commands_in.end(); i++) {
+    ros::NodeHandle Iris; // subscriber
+    if (Iris.getParam("/autonomous", commands_in))
+    {
+        for (auto i = commands_in.begin(); i != commands_in.end(); i++)
+        {
             commands[AUTOCMD_STRING_TO_ENUM[i->first]] = i->second;
         }
         ROS_INFO("Autonomous commands have been loaded for camera.");
     }
-    else {
+    else
+    {
         ROS_ERROR("You must load autonomous commands before using camera.");
         return 1;
     }
-    ros::NodeHandle Cornea; //publisher
+    if (Iris.getParam("/camera", commands_in))
+    {
+        for (auto i = commands_in.begin(); i != commands_in.end(); i++)
+        {
+            switch (i->first)
+            {
+            case "topBandWidth":
+                topBandWidth = i->second;
+                break;
+            case "sideBandWidth":
+                sideBandWidth = i->second;
+                break;
+            case "imageWidth":
+                imageWidth = i->second;
+                break;
+            case "imageHeight":
+                imageHeight = i->second;
+                break;
+            case "brightnessThreshold":
+                brightnessThreshold = i->second;
+                break;
+            case "sidePercentThreshold":
+                sidePercentThreshold = i->second;
+                break;
+            case "topPercentThreshold":
+                topPercentThreshold = i->second;
+                break;
+            case "numMiddlePixels":
+                numMiddlePixels = i->second;
+                break;
+            default:
+                ROS_ERROR("Unexpected variable: " + String(i->second));
+            }
+        }
+        ROS_INFO("Loaded parameter: " + String(i->first) + " " + String(i->second));
+    }
+
+    ros::NodeHandle Cornea;                                                                // publisher
     ros::Subscriber Handle = Iris.subscribe("/cv_camera/image_mono", 10, chatterCallBack); // 10 was 1000
     ros::Subscriber eoc_sub = Iris.subscribe("end_of_choreo", 1000, pauseCallback);
-    //george = Cornea.advertise<std_msgs::String>("cameron", 1000);
+    // george = Cornea.advertise<std_msgs::String>("cameron", 1000);
     george = Iris.advertise<std_msgs::String>("cameron", 10); // 10 was 1000
-    //ros::spin();
+    // ros::spin();
     debug = Iris.advertise<std_msgs::String>("cam_debug", 10);
     notify_lidar = Iris.advertise<std_msgs::UInt8>("camera_to_lidar", 1000);
 
-    // Fee copied over from simple_motors trigger 
+    // Fee copied over from simple_motors trigger
     ros::Rate delay_rate(5);
-    
-    while(ros::ok()) {
-	
+
+    while (ros::ok())
+    {
     }
 }
 
-void pauseCallback(const std_msgs::Empty empty_msg) {
-	listening = true;
+void pauseCallback(const std_msgs::Empty empty_msg)
+{
+    listening = true;
 }
 
-void chatterCallBack(const sensor_msgs::Image& view)
+void chatterCallBack(const sensor_msgs::Image &view)
 {
     double topCount = 0;
     double leftCount = 0;
     double rightCount = 0;
     double middleCount = 0;
-    
+
     imageWidth = view.width;
     imageHeight = view.height;
 
-    for(int i = 0; i < view.data.size(); i++) //grab top
+    for (int i = 0; i < view.data.size(); i++) // grab top
     {
-    //std::string command = "i: " + std::to_string(i);
-    
-        if(inTop(i))
+        // std::string command = "i: " + std::to_string(i);
+
+        if (inTop(i))
         {
-            //command += "T";
-            topCount += (view.data[i] > threshold); 
+            // command += "T";
+            topCount += (view.data[i] > brightnessThreshold);
         }
-        if(inLeft(i))
+        if (inLeft(i))
         {
-        //command += "L";
-            leftCount += (view.data[i] > threshold);
+            // command += "L";
+            leftCount += (view.data[i] > brightnessThreshold);
         }
-        if(inRight(i))
+        if (inRight(i))
         {
-        //command += "R";
-            rightCount += (view.data[i] > threshold);
+            // command += "R";
+            rightCount += (view.data[i] > brightnessThreshold);
         }
-        if (inMiddle(i)) {
-            middleCount += (view.data[i] > threshold);
+        if (inMiddle(i))
+        {
+            middleCount += (view.data[i] > brightnessThreshold);
         }
     }
-    double leftAverage = leftCount/ (imageHeight*sideBandWidth);
-    double rightAverage = rightCount / (imageHeight*sideBandWidth);
-    double topAverage = topCount / (imageWidth*topBandWidth);
-    double middleAverage = middleCount / num_middle_pixels;
+    double leftAverage = leftCount / (imageHeight * sideBandWidth);
+    double rightAverage = rightCount / (imageHeight * sideBandWidth);
+    double topAverage = topCount / (imageWidth * topBandWidth);
+    double middleAverage = middleCount / numMiddlePixels;
 
     std::string result;
-
 
     /*
         forward = f
         backward = b
-        stop = s 
+        stop = s
         pivotr = R
         pivotl = L
         fwdl = l
         fwdr = r
         bwdl = e
         bwdr = i
-        
+
     */
 
     std_msgs::UInt8 camera_to_lidar_msg;
-    camera_to_lidar_msg.data = 4 * (topAverage > top_percent_threshold) + 2 * (rightAverage > side_percent_threshold) + 1 * (leftAverage > side_percent_threshold);
+    camera_to_lidar_msg.data = 4 * (topAverage > topPercentThreshold) + 2 * (rightAverage > sidePercentThreshold) + 1 * (leftAverage > sidePercentThreshold);
     notify_lidar.publish(camera_to_lidar_msg);
 
-    if (!listening) return;
+    if (!listening)
+        return;
 
-    if(topAverage > top_percent_threshold && leftAverage > side_percent_threshold && rightAverage > side_percent_threshold) // white in top, left, right
+    if (topAverage > topPercentThreshold && leftAverage > sidePercentThreshold && rightAverage > sidePercentThreshold) // white in top, left, right
     {
-        if (favorRight) {
+        if (favorRight)
+        {
             result = commands[PIVOTR];
-        } else {
+        }
+        else
+        {
             result = commands[PIVOTL];
         }
-        
     }
-    else if(topAverage > top_percent_threshold && leftAverage > side_percent_threshold && rightAverage <= side_percent_threshold) // white in top, left
+    else if (topAverage > topPercentThreshold && leftAverage > sidePercentThreshold && rightAverage <= sidePercentThreshold) // white in top, left
     {
         result = commands[RCP]; // PIVOTR;
-	listening = false;
+        listening = false;
         favorRight = true;
     }
-    else if(topAverage > top_percent_threshold && leftAverage <= side_percent_threshold && rightAverage > side_percent_threshold) // white in top, right
+    else if (topAverage > topPercentThreshold && leftAverage <= sidePercentThreshold && rightAverage > sidePercentThreshold) // white in top, right
     {
         result = commands[LCP]; // PIVOTL;
-	listening = false;
+        listening = false;
         favorRight = false;
     }
-    else if(topAverage > top_percent_threshold && leftAverage <= side_percent_threshold && rightAverage <= side_percent_threshold) // white in top
+    else if (topAverage > topPercentThreshold && leftAverage <= sidePercentThreshold && rightAverage <= sidePercentThreshold) // white in top
     {
-        if (favorRight) {
+        if (favorRight)
+        {
             result = commands[PIVOTR];
         }
-        else {
+        else
+        {
             result = commands[PIVOTL];
         }
     }
-    else if(topAverage <= top_percent_threshold && leftAverage > side_percent_threshold && rightAverage > side_percent_threshold) // white in left, right
+    else if (topAverage <= topPercentThreshold && leftAverage > sidePercentThreshold && rightAverage > sidePercentThreshold) // white in left, right
     {
         result = commands[FWD];
     }
-    else if(topAverage <= top_percent_threshold && leftAverage > side_percent_threshold && rightAverage <= side_percent_threshold) // white in left
+    else if (topAverage <= topPercentThreshold && leftAverage > sidePercentThreshold && rightAverage <= sidePercentThreshold) // white in left
     {
         result = commands[VEERR];
         favorRight = true;
     }
-    else if(topAverage <= top_percent_threshold && leftAverage <= side_percent_threshold && rightAverage > side_percent_threshold) // white in right
+    else if (topAverage <= topPercentThreshold && leftAverage <= sidePercentThreshold && rightAverage > sidePercentThreshold) // white in right
     {
         result = commands[VEERL];
         favorRight = false;
     }
     else // no white
     {
-        result = commands[GO]; //f = fwd but with lower priority
+        result = commands[GO]; // f = fwd but with lower priority
     }
-     
-    if (middleAverage > top_percent_threshold) { // crossed over line --> backup and then pivot
-	listening = true; // added in case choreo is overwritten by FPIVOTX
-        if (favorRight) {
+
+    if (middleAverage > topPercentThreshold)
+    {                     // crossed over line --> backup and then pivot
+        listening = true; // added in case choreo is overwritten by FPIVOTX
+        if (favorRight)
+        {
             result = commands[FPIVOTR];
         }
-        else {
+        else
+        {
             result = commands[FPIVOTL];
         }
     }
 
-
     std::string d;
     // TODO: comment out when ready to send to queue
-    // 
-    d = "Top: "; 
+    //
+    d = "Top: ";
     d += std::to_string(topAverage);
-    d += " Left: "; 
+    d += " Left: ";
     d += std::to_string(leftAverage);
-    d += " Right: "; 
+    d += " Right: ";
     d += std::to_string(rightAverage);
-    d += " Middle: "; 
+    d += " Middle: ";
     d += std::to_string(middleAverage);
-	d += "\nThreshold: "; 
+    d += "\nThreshold: ";
     d += std::to_string(threshold);
-    d += " Top Threshold: "; 
-    d += std::to_string(top_percent_threshold);
-    d += " Side Threshold: "; 
-    d += std::to_string(side_percent_threshold);
+    d += " Top Threshold: ";
+    d += std::to_string(topPercentThreshold);
+    d += " Side Threshold: ";
+    d += std::to_string(sidePercentThreshold);
 
     std_msgs::String stuff;
     stuff.data = result;
@@ -227,13 +275,12 @@ void chatterCallBack(const sensor_msgs::Image& view)
     other.data = d;
     debug.publish(other);
 
-    data_read = true; // now compare values after the first frame 
-
+    data_read = true; // now compare values after the first frame
 }
 
 bool inTop(int counter)
 {
-    return counter < (imageWidth*topBandWidth);
+    return counter < (imageWidth * topBandWidth);
 }
 
 bool inLeft(int counter)
