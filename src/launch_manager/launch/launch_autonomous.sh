52ca377f72f609cb6b2f9780ff411cfc7c60b52d
froggyhopper50 <jnkennedy32@comcast.net>
2022-09-17 21:18:48 -0400

    took out the calls to pre.sh in launch_autonomous.sh. Still needs to be tweaked further

--- src/launch_manager/launch/launch_autonomous.sh
+++ src/launch_manager/launch/launch_autonomous.sh
@@ -4,7 +4,7 @@ cd ~/anon_auton_ws
 # cd /Users/felichri/Documents/AnonAuton/anon_auton_ws
 source devel/setup.bash
 
-/bin/bash ~/anon_auton_ws/src/launch_manager/launch/pre.sh
+#/bin/bash ~/anon_auton_ws/src/launch_manager/launch/pre.sh
 
 # wait until roscore is running
 until rostopic list ; do sleep 1; done
@@ -13,7 +13,7 @@ cd ~/anon_auton_ws/src/launch_manager/launch/components
 
 if grep -iq "^antenna_port" ~/anon_auton_ws/src/config_manager/configs/ports/active.yaml; then
     echo "---------------- ANTENNA found ----------------"
-    roslaunch --wait antenna_chair.launch &
+#    roslaunch --wait antenna_chair.launch &
 else
     echo "!!! NO ANTENNA FOUND !!!"
 fi
