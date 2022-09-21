82a41f9f0d1caca45c7a96387b5ed27bc4a9305d
Fee Christoph <felichri@umich.edu>
2022-09-20 22:06:23 -0400

    real file

--- src/launch_manager/launch/reset.sh
+++ src/launch_manager/launch/reset.sh
@@ -6,4 +6,4 @@ source devel/setup.bash
 ./src/launch_manager/launch/kill_ros.sh
 ./src/launch_manager/launch/pre.sh &
 
-./src/launch_manager/launch/components/antenna_chair.sh &
+./src/launch_manager/launch/launch_antenna_chair.sh &
