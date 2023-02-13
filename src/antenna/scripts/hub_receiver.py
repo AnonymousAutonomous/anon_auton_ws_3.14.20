56e9407d8515d83ad739b798599f82b37bda6e43
Fee Christoph <felichri@umich.edu>
2023-02-13 12:33:29 -0500

    strip newline

--- src/antenna/scripts/hub_receiver.py
+++ src/antenna/scripts/hub_receiver.py
@@ -12,7 +12,7 @@ def hub_receiver():
     rospy.init_node("hub_receiver", anonymous=True)
     ser = serial.Serial(antenna_port, 57600)
     while not rospy.is_shutdown():
-        str_msg = ser.readline()
+        str_msg = ser.readline().strip()
         # [2:].decode("utf-8").strip()
         rospy.loginfo(str_msg)
         pub.publish(str_msg)
