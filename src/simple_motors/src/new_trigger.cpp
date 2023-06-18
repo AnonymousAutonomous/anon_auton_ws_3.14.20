034f83dd34dc7f620d239fb4c329ce8101d9c762
Fee Christoph <felichri@umich.edu>
2023-06-18 16:03:16 -0400

    cast to char

--- src/simple_motors/src/new_trigger.cpp
+++ src/simple_motors/src/new_trigger.cpp
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
     }
     else
     {
-      ROS_INFO("Custom: " + cmd + " " + char(cmd.length()));
+      ROS_INFO("Custom: " + cmd + " " + ('0' + cmd.length()));
 
       if (cmd.length() == 8 &&
           (cmd[0] == 'f' || cmd[0] == 'r') &&
