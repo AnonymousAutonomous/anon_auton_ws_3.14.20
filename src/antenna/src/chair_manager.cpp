e7c2f33550c0f2a911214b55c544f4d0a190026d
Fee Christoph <felichri@umich.edu>
2023-09-23 16:42:32 -0400

    reinterpret cast

--- src/antenna/src/chair_manager.cpp
+++ src/antenna/src/chair_manager.cpp
@@ -150,7 +150,9 @@ void receive_image_callback(const sensor_msgs::Image &view)
 	chair_manager_pub.publish(msg_debug);
 
 	std::string prefix = "image " + std::to_string(view.height) + " " + std::to_string(view.width) + " ";
-	std::string data_str(view.data, sizeof(view.data));
+	// std::string data_str(view.data, sizeof(view.data));
+
+	std::string data_str = reinterpret_cast<char *>(view.data);
 
 	std_msgs::String msg;
 	msg.data = data_str;
