3cb4f54e4157b4f982db0e9aa54dbec24ac58a79
Fee Christoph <felichri@umich.edu>
2023-09-23 17:53:20 -0400

    missing semicolon

--- src/antenna/src/chair_manager.cpp
+++ src/antenna/src/chair_manager.cpp
@@ -147,8 +147,8 @@ void receive_image_callback(const sensor_msgs::Image &view)
 	std_msgs::String msg_debug;
 	msg_debug.data = "received image callback " + std::to_string(view.height) + " " + std::to_string(view.width) + " " + view.encoding;
 
-	chair_manager_pub.publish(msg_debug)
-		std::string prefix = "image " + std::to_string(view.height) + " " + std::to_string(view.width) + " ";
+	chair_manager_pub.publish(msg_debug);
+	std::string prefix = "image " + std::to_string(view.height) + " " + std::to_string(view.width) + " ";
 	// std::string data_str(view.data, sizeof(view.data));
 
 	// std::string data_str = reinterpret_cast<char *>(&view.data);
