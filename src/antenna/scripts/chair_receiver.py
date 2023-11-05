7d8407a2f36aabaf9b91b52cfdcf7a8d2f99d462
Fee Christoph <felichri@umich.edu>
2023-11-05 18:19:50 -0500

    clean up global ser

--- src/antenna/scripts/chair_receiver.py
+++ src/antenna/scripts/chair_receiver.py
@@ -6,6 +6,7 @@ from std_msgs.msg import String, Empty
 
 antenna_port = rospy.get_param('antenna_port')
 chair_num = rospy.get_param('chair_num')
+ser = serial.Serial(antenna_port, 57600)
 
 
 # def close_callback(data, ser):
@@ -13,18 +14,17 @@ chair_num = rospy.get_param('chair_num')
 #     rospy.signal_shutdown("Received shutdown ")
 
 
-def close_serial(ser):
+def close_serial():
     rospy.logerror("SHUTTING DOWN ANTENNA RECEIVER")
     ser.close()
 
 def chair_receiver():
-    ser = serial.Serial(antenna_port, 57600)
     pub = rospy.Publisher("from_chair_receiver", String, queue_size=10)
     debug_pub = rospy.Publisher("chair_receiver_debug", String, queue_size=10)
     # rospy.Subscriber('shutdown_ros', Empty, close_callback, (ser))
     rospy.init_node("chair_receiver", anonymous=False)
 
-    rospy.on_shutdown(close_serial, (ser))
+    rospy.on_shutdown(close_serial)
 
     while not rospy.is_shutdown():
         str_msg = ser.readline().strip()
