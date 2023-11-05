20f7747b67ea861b28154806624044c737dac76e
Fee Christoph <felichri@umich.edu>
2023-11-05 17:49:09 -0500

    try with open as ser

--- src/antenna/scripts/chair_receiver.py
+++ src/antenna/scripts/chair_receiver.py
@@ -6,25 +6,25 @@ from std_msgs.msg import String, Empty
 
 antenna_port = rospy.get_param('antenna_port')
 chair_num = rospy.get_param('chair_num')
-ser = serial.Serial(antenna_port, 57600)
 
 
-def close_callback(data):
-    ser.close()
+# def close_callback(data):
+#     ser.close()
 
 
 def chair_receiver():
     pub = rospy.Publisher("from_chair_receiver", String, queue_size=10)
     debug_pub = rospy.Publisher("chair_receiver_debug", String, queue_size=10)
-    rospy.Subscriber('shutdown_ros', Empty, close_callback)
+    # rospy.Subscriber('shutdown_ros', Empty, close_callback)
     rospy.init_node("chair_receiver", anonymous=False)
 
     while not rospy.is_shutdown():
-        str_msg = ser.readline().strip()
-        rospy.loginfo(str_msg)
-        debug_pub.publish(str_msg)
-        if str_msg[0] == chair_num or str_msg[0] == "0":
-            pub.publish(str_msg[1:])
+        with serial.Serial(antenna_port, 57600) as ser:
+            str_msg = ser.readline().strip()
+            rospy.loginfo(str_msg)
+            debug_pub.publish(str_msg)
+            if str_msg[0] == chair_num or str_msg[0] == "0":
+                pub.publish(str_msg[1:])
 
 
 if __name__ == '__main__':
