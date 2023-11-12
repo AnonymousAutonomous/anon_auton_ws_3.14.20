ad69f45f70025e0fe9c3e9077e4f8825140ac7e0
Fee Christoph <felichri@umich.edu>
2023-11-12 16:14:57 -0500

    dont wait for shutdown

--- src/antenna/src/chair_manager.cpp
+++ src/antenna/src/chair_manager.cpp
@@ -225,7 +225,6 @@ int main(int argc, char **argv)
 	chair_manager_pub = nh.advertise<std_msgs::String>("driver_output", 1000);
 	test_pub = nh.advertise<std_msgs::String>("from_chair", 1000);
 
-	ros::waitForShutdown();
 	ros::Rate delay_rate(5); // 5 cycles per second
 
 	while (ros::ok())
