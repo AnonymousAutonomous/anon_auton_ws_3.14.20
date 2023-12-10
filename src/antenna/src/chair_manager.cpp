398b215ddd34f6b67cc3a54633d44918a668dee4
Fee Christoph <felichri@umich.edu>
2023-12-09 20:34:47 -0500

    spin instead of spinonce

--- src/antenna/src/chair_manager.cpp
+++ src/antenna/src/chair_manager.cpp
@@ -276,7 +276,7 @@ int main(int argc, char **argv)
 		// 	msg.data = 'h'; // heartbeat!
 		// 	from_chair_pub.publish(msg);
 		// 	delay_rate.sleep(); // runs out duration is remaining
-		ros::spinOnce();
+		ros::spin();
 	}
 	ros::waitForShutdown();
 }
