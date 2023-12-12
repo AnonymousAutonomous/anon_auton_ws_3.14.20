a936e12fe0ef603d928d89ee18e15de886c3ee42
Fee Christoph <felichri@umich.edu>
2023-12-11 21:13:18 -0500

    back to double

--- src/eyes/src/baby_trilogy.cpp
+++ src/eyes/src/baby_trilogy.cpp
@@ -73,7 +73,7 @@ void updateStuckStatus()
 		ros::WallTime earliest_choreo = lidar_stuck_pq.top();
 		// ros::WallTime now = ros::WallTime::now();
 		// double now = ros::WallTime::now().toSec();
-		ROS_ERROR("LIDAR DIFF: %d\t%d\t%d", static_cast<float64>(ros::WallTime::now().toSec()), static_cast<float64>(earliest_choreo.toSec()), static_cast<float64>((ros::WallTime::now() - earliest_choreo).toSec()));
+		ROS_ERROR("LIDAR DIFF: %d\t%d\t%d", static_cast<double>(ros::WallTime::now().toSec()), static_cast<double>(earliest_choreo.toSec()), static_cast<double>((ros::WallTime::now() - earliest_choreo).toSec()));
 		if ((ros::WallTime::now() - earliest_choreo).toSec() <= camera_trapped_duration)
 		{
 			std_msgs::Char msg;
