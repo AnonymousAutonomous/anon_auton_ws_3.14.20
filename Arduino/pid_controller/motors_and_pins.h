e9edd964616e1d68cafceb24c6e66f9bbc249786
Fee Christoph <felichri@umich.edu>
2024-01-16 13:36:04 -0800

    reverse motors in pid controller code

--- Arduino/pid_controller/motors_and_pins.h
+++ Arduino/pid_controller/motors_and_pins.h
@@ -10,10 +10,10 @@
 #define BWD 'r'
 #define STOP 's'
 
-#define REVERSE_A false
-#define REVERSE_B false
+#define REVERSE_A true
+#define REVERSE_B true
 
-//define motor 1 related pins (right)
+// define motor 1 related pins (right)
 #define AIN1_tmp 8
 #define AIN2_tmp 9
 #define PWMA 11
@@ -22,11 +22,10 @@
 const int AIN1 = REVERSE_A ? AIN2_tmp : AIN1_tmp;
 const int AIN2 = REVERSE_A ? AIN1_tmp : AIN2_tmp;
 
-
-#define ENCA 3 // yellow, INTERRUPT
+#define ENCA 3  // yellow, INTERRUPT
 #define STBYA 6 // white
 
-//define motor 2 related pins (left)
+// define motor 2 related pins (left)
 #define BIN1_tmp 7
 #define BIN2_tmp 12
 #define PWMB 10
@@ -35,73 +34,83 @@ const int AIN2 = REVERSE_A ? AIN1_tmp : AIN2_tmp;
 const int BIN1 = REVERSE_B ? BIN2_tmp : BIN1_tmp;
 const int BIN2 = REVERSE_B ? BIN1_tmp : BIN2_tmp;
 
-                          
-#define ENCB 2 // yellow, INTERRUPT
+#define ENCB 2  // yellow, INTERRUPT
 #define STBYB 5 // white
 
 #define ENCODER_CONVERSION 309.523809524
 
 // PWM
 #define ANALOG_WRITE_BITS 8
-#define MAX_PWM pow(2, ANALOG_WRITE_BITS)-1
-#define MIN_PWM 0    // Let motors stop
-
-
+#define MAX_PWM pow(2, ANALOG_WRITE_BITS) - 1
+#define MIN_PWM 0 // Let motors stop
 
 /***********************************************************
  * HELPER FUNCTIONS                                        *
  ***********************************************************/
-void setADir(char dir) {
-  if (dir == BWD) {
+void setADir(char dir)
+{
+  if (dir == BWD)
+  {
     digitalWrite(AIN1, LOW);
     digitalWrite(AIN2, HIGH);
   }
-  else if (dir == FWD) {
+  else if (dir == FWD)
+  {
     digitalWrite(AIN1, HIGH);
     digitalWrite(AIN2, LOW);
   }
-  else { // STOP / BRAKE
+  else
+  { // STOP / BRAKE
     digitalWrite(AIN1, LOW);
     digitalWrite(AIN2, LOW);
   }
 };
 
-void setBDir(char dir) {
-  if (dir == BWD) {
+void setBDir(char dir)
+{
+  if (dir == BWD)
+  {
     digitalWrite(BIN1, LOW);
     digitalWrite(BIN2, HIGH);
   }
-  else if (dir == FWD) {
+  else if (dir == FWD)
+  {
     digitalWrite(BIN1, HIGH);
     digitalWrite(BIN2, LOW);
   }
-  else { // STOP / BRAKE
+  else
+  { // STOP / BRAKE
     digitalWrite(BIN1, LOW);
     digitalWrite(BIN2, LOW);
   }
 };
 
-void moveA(uint16_t pwm){
+void moveA(uint16_t pwm)
+{
   analogWrite(PWMA, pwm);
 };
 
-void moveB(uint16_t pwm){
+void moveB(uint16_t pwm)
+{
   analogWrite(PWMB, pwm);
 };
 
-void standbyMotors(bool standby){
-  if (standby == true){
+void standbyMotors(bool standby)
+{
+  if (standby == true)
+  {
     digitalWrite(STBYA, LOW);
     digitalWrite(STBYB, LOW);
   }
-  else{
+  else
+  {
     digitalWrite(STBYA, HIGH);
     digitalWrite(STBYB, HIGH);
   }
 };
 
-
-void initMotors(){
+void initMotors()
+{
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(PWMA, OUTPUT);
@@ -110,10 +119,8 @@ void initMotors(){
   pinMode(PWMB, OUTPUT);
   pinMode(STBYA, OUTPUT);
   pinMode(STBYB, OUTPUT);
-//   analogWriteResolution(ANALOG_WRITE_BITS);
+  //   analogWriteResolution(ANALOG_WRITE_BITS);
   standbyMotors(false);
 };
-
-
 
 #endif
