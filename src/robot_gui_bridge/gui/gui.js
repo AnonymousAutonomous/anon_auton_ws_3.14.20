e2dea54056a97897656ab6c9644a05516c9c8586
Fee Christoph <felichri@umich.edu>
2024-01-16 19:32:46 -0800

    i hate this

--- src/robot_gui_bridge/gui/gui.js
+++ src/robot_gui_bridge/gui/gui.js
@@ -51,8 +51,6 @@ function handleSetActiveChairs(e) {
     
     // Set chair list. Handle rest of updates in the getter for this param, after we know the param is updated.
     active_chair_nums.set(newActiveChairs);
-    // Reload new vals?
-    active_chair_nums.get(setActiveChairNums);
 
     modal.style.display = "none";
 }
