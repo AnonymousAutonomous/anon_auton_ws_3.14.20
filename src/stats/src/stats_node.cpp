49035cd431e0b814ca99cb0c99e01c0deb4e95f3
Fee Christoph <felichri@umich.edu>
2024-01-22 16:17:26 -0800

    5 seconds delay instead of 10

--- src/stats/src/stats_node.cpp
+++ src/stats/src/stats_node.cpp
@@ -13,7 +13,7 @@
 #include <string>
 
 std::vector<std::string> topics_we_care_about{"/cv_camera/image_mono", "/raw_obstacles"};
-ros::Duration timeBeforeOfflineSec(10); // 10 seconds
+ros::Duration timeBeforeOfflineSec(5); // 5 seconds
 
 std::unordered_map<std::string, ros::Time> topic_to_last_start_time = {};
 
